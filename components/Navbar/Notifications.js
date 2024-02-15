cba7ad37e77d0f60698a1905ba88b8d6cd0454c3
Salah-Moftah <132005420+Salah-Moftah@users.noreply.github.com>
2024-02-15 11:32:46 +0200

    Update Notifications.js

--- components/Navbar/Notifications.js
+++ components/Navbar/Notifications.js
@@ -3,7 +3,7 @@ import { IoNotifications } from "react-icons/io5";
 function Notifications() {
   return (
     <div className="relative cursor-pointer mr-2">
-      <IoNotifications className='text-accent-color w-[29px] h-[29px]' />
+      <IoNotifications className='text-accent-color w-[30px] h-[30px]' />
       <div className="w-[16px] h-[16px] bg-green-color rounded-full 
       text-white font-bold text-[12px] flex justify-center items-center absolute
       top-0 right-[-2px]">
