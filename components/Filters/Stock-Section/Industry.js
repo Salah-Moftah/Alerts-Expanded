74bc14672645383c075c9d2bcc279d0fffe905f8
Salah-Moftah <132005420+Salah-Moftah@users.noreply.github.com>
2024-02-15 12:30:13 +0200

    Update Industry.js

--- components/Filters/Stock-Section/Industry.js
+++ components/Filters/Stock-Section/Industry.js
@@ -6,15 +6,15 @@ function Industry() {
   return (
     <div>
       <HeadLineStock title="Industry" />
-      <ul className="md:grid md:grid-cols-2 flex items-center flex-col mt-3">
+      <ul className="md:grid md:grid-cols-2 flex items-center gap-3 md:gap-0 flex-col mt-3">
         {industryList.map((el) => (
           <li
             key={el.title}
             className="h-[25px] md:border-l p-3 md:pb-4 pb-6 text-white text-[12px]
           font-normal  w-fit"
           >
-            <div className="flex items-center gap-2 px-1 rounded-[6px] hover:bg-accent-color hover:text-first-gray">
-              <img src={el.img} alt="photo" className="w-[13px]" />
+            <div className="text-center flex items-center gap-2 px-1 rounded-[6px] hover:bg-accent-color hover:text-first-gray">
+              <img src={el.img} alt="photo" className="w-[13px]  md:block hidden" />
               <span>{el.title}</span>
             </div>
           </li>
