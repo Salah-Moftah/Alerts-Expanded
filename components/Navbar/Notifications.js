1f164de81b0fcf7cd03e60565f08b76e55aa7022
Salah-Moftah <132005420+Salah-Moftah@users.noreply.github.com>
2024-02-15 11:31:13 +0200

    Update Notifications.js

--- components/Navbar/Notifications.js
+++ components/Navbar/Notifications.js
@@ -4,7 +4,7 @@ function Notifications() {
   return (
     <div className="relative cursor-pointer mr-2">
       <IoNotifications className='text-accent-color w-[29px] h-[29px]' />
-      <div className="w-[15px] h-[15px] bg-green-color rounded-full 
+      <div className="w-[16px] h-[16px] bg-green-color rounded-full 
       text-white font-bold text-[12px] flex justify-center items-center absolute
       top-0 right-[-2px]">
         6
