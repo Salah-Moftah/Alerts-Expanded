70b2fcf5baf4aac30cd9390f76cb82aa20593f03
Salah-Moftah <132005420+Salah-Moftah@users.noreply.github.com>
2024-02-15 12:15:45 +0200

    Update page.js

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import { OpenFilterProvider } from "@/contexts/openFilterContext";
 export default function Home() {
   return (
     <OpenFilterProvider>
-      <main className="flex items-start gap-10">
+      <main className="flex items-start gap-5 md:gap-10">
         <Sidebar />
         <div className="py-6 flex-1 gap-6 ml-32 overflow-hidden h-screen relative mid-section">
           <Navbar />
